a643803d6137b552915a164926e237a623725188
Arganz-xd <arganztea.ans@gmail.com>
2025-01-23 08:17:11 +0700

    Mengubah font dan clean code

---
+++ script.js
@@ -0,0 +1,7 @@
+const text = document.getElementById("typing");
+let typed = new Typed(text, {
+  strings: ["Fresh Graduate","Web Developer"],
+  typeSpeed: 100,
+  backSpeed: 100,
+  loop: true
+});
